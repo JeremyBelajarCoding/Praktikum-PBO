b6d8fe711ef79fd09a44417af396b38c9a077112
JeremyBelajarCoding <jeremymsp0812@gmail.com>
2026-09-08 12:05:39 +0700

    3.2.

--- Jobsheet3/Motor.java
+++ Jobsheet3/Motor.java
@@ -1,11 +1,11 @@
 package Jobsheet3;
 
 public class Motor {
-    public String platNomor;
-    public boolean isMesinOn;
-    public int kecepatan;
+    private String platNomor;
+    private boolean isMesinOn;
+    private int kecepatan;
 
-    public void displayStatus() {
+public void displayStatus() {
         System.out.println("Plat Nomor: " + this.platNomor);
         if (isMesinOn) {
             System.out.println("Mesin On");
@@ -14,6 +14,33 @@ public class Motor {
         }
         System.out.println("Kecepatan: " + this.kecepatan);
         System.out.println("=====");
-        
+    }
+
+    public String getPlatNomor() {
+        return platNomor;
+    }
+
+    public void setPlatNomor(String platNomor) {
+        this.platNomor = platNomor;
+    }
+
+    public boolean getIsMesinOn() {
+        return isMesinOn;
+    }
+
+    public void setIsMesinOn(boolean isMesinOn) {
+        this.isMesinOn = isMesinOn;
+    }
+
+    public int getKecepatan() {
+        return kecepatan;
+    }
+
+    public void setKecepatan(int kecepatan) {
+        if (!this.isMesinOn && kecepatan > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else {
+            this.kecepatan = kecepatan;
+        }
     }
 }
